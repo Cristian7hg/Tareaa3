159a15f54728f38fad48547147698b2e1055fbfa
Cristian Heredia <20221864@itla.edu.do>
2024-11-22 18:57:05 -0400

    hi

--- Sale.Web/obj/Release/net6.0/Sale.Web.AssemblyInfo.cs
+++ Sale.Web/obj/Release/net6.0/Sale.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sale.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f065316092b193a489d90a172420ea9b198d4176")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sale.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sale.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
